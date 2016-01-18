a9bd8040c2c5d8dbd124d794660265c148cd43c2
Zach Sullivan <zsulliva@indiana.edu>
2016-01-18 18:36:39 -0500

    flatten

--- compiler.rkt
+++ compiler.rkt
@@ -21,35 +21,86 @@
 
 (define uniquify (uniquify^ '()))
 
-(define (flatten e)
-  (match e
-    [(? integer?) (values e '())]
-    [(? symbol?)  (values e '())]
-    [`(let ([,x ,e]) ,body)
-     (let-values ([(val ass) (flatten e)])
-       (let-values ([(val^ ass^) (flatten body)])
-         (values val^ (append ass (append ass^ `((assign ,x ,e)))))))]
-    [`(program ,e)
-     (let-values ([(val ass) (flatten e)])
-       `(program ,@(append ass `((return ,val)))))]))
+(define (flatten^ need-arg)
+  (lambda (e)
+    (match e
+      [(? integer?) (values e '())]
+      [(? symbol?)  (values e '())]
+      [`(let ([,x ,e]) ,body)
+        (cond
+          [need-arg
+            (let-values ([(expr ass) ((flatten^ #t) e)])
+              (let-values ([(expr^ ass^) ((flatten^ #f) body)])
+                (values expr^
+                        (append ass ass^ `((assign ,x ,expr))))))]
+          [else
+           (let-values ([(expr ass) ((flatten^ need-arg) e)])
+              (values expr ass))])]
+      [`(program ,e)
+        (let-values ([(expr ass) ((flatten^ need-arg) e)])
+          `(program ,expr ,@(append ass `((return ,expr)))))]
+      [`(,op ,es ...)
+        (cond
+          [need-arg
+           (let ([temp (gensym)])
+             (let-values ([(exprs asses) (map2 (flatten^ need-arg) es)])
+               (values temp
+                       (append asses
+                               `((assign ,temp (,op ,@exprs)))))))]
+          [else
+            (let-values ([(exprs asses) (map2 (flatten^ need-arg) es)])
+              (values `(,op ,@exprs)
+                      asses))])]
+      )))
 
-(println `(Flatten Tests -----------------------------------))
-(flatten `(program 42))
-(flatten `(program (let ([x 56]) x)))
-(flatten `(program (let ([x 56])
-                     (let ([y 42])
-                       y))))
-;; (flatten `(program `(let ([x 2]) x)))
-;; (flatten `(program 22))
-;; ((flatten '()) `(program (+ 52 (- 10))))
-;; ((flatten '()) `(program
-;;                   (let ([x (+ (- 10 11))])
-;;                     (+ x 41))))
-;; ((flatten '()) `(program
-;;                   (let ([a 24])
-;;                     (let ([b a])
-;;                       b))))
 
+(define flatten
+  (lambda (e)
+    (match e
+      [(? symbol?) (values e '())]
+      [(? integer?) (values e '())]
+      [`(let ([,x ,rhs]) ,body)
+       (let-values ([(rhs_ex rhs_stmts) (flatten rhs)]
+                    [(body_ex body_stmts) (flatten body)])
+         (values body_ex (append rhs_stmts body_stmts `((assign ,x ,rhs_ex)))))]    
+      [`(program ,e) (let-values ([(ex rhs) (flatten e)])
+                       (let ([vars (unique-vars rhs)])
+                         `(program ,vars ,@(append rhs `((return ,ex))))))]
+      [`(read) (let ([tmp (gensym)])
+                 (values tmp `((assign ,tmp (read)))))]
+      [`(- ,es) (let ([tmp (gensym)])
+                  (let-values ([(ex stmts) (flatten es)])
+                    (values tmp (append stmts `((assign ,tmp (- ,ex)))))))]
+      [`(+ ,e1 ,e2) (let ([tmp (gensym)])
+                      (let-values ([(ex1 stmts1) (flatten e1)]
+                                   [(ex2 stmts2) (flatten e2)])
+                         (values tmp (append stmts1 stmts2 `((assign ,tmp (+ ,ex1 ,ex2)))))))]
+           )))
+
+(define unique-vars
+  (lambda (e)
+    (cond
+      [(null? e) '()]
+      [else (match (car e)
+                   [`(assign ,e1 ,e2) (cons e1 (unique-vars (cdr e)))]
+                   )])))
+
+
+;; (display "Flatten Tests -----------------------------------")
+;; (newline)
+;; (flatten `(program 42))
+;; (newline)
+;; (flatten `(program (+ 1 2)))
+;; (newline)
+;; (flatten `(program
+;;             (let ([x (+ (- 10) 11)])
+;;               (+ x 41))))
+;; (newline)
+;; (flatten `(program (+ (- 1) (- 2))))
+;; (newline)
+;; (flatten `(program (let ([x 56])
+;;                      (let ([y x])
+;;                        y))))
 
 (define (selection-instruction e) e)
 (define (assign-homes e) e)
@@ -57,7 +108,7 @@
 (define (print-x86 e) e)
 
 (define r1-passes `(("uniquify",uniquify,interp-scheme)
-                    ;; ("flatten",flatten,interp-C)
+                    ("flatten",flatten,interp-C)
                     ;; ("select instructions",selection-instruction,interp-x86)
                     ;; ("assign homes",assign-homes,interp-x86)
                     ;; ("patch instructions",patch-instructions,interp-x86)
