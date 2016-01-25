ff699825a34219486ae64f3a21d837af844493d2
Zach Sullivan <zsulliva@indiana.edu>
2016-01-25 09:18:27 -0500

    uncover-live complete

--- passes/uncover-live.rkt
+++ passes/uncover-live.rkt
@@ -34,23 +34,23 @@
     (match instr
      [`(,_ ,_ (,_ ,b)) b])))
 
-((lambda (p)
-   (uncover-live p))
- `(program (y)
-    (movq (int 1) (var y))
-    (subq (var y) (reg rax))))
+;; ((lambda (p)
+;;    (uncover-live p))
+;;  `(program (y)
+;;     (movq (int 1) (var y))
+;;     (subq (var y) (reg rax))))
 
 
-((lambda (p)
-   (uncover-live p))
- `(program (v w x y z)
-    (movq (int 1) (var v))       ;; v
-    (movq (int 46) (var w))      ;; v,w
-    (movq (var v) (var x))       ;; w,x
-    (addq (int 7) (var x))       ;; w,x
-    (movq (var x) (var y))       ;; w,x,y
-    (addq (int 4) (var y))       ;; w,x,y
-    (movq (var x) (var z))       ;; w,y,z
-    (addq (var w) (var z))       ;; y,z
-    (movq (var z) (reg rax))     ;; y,rax
-    (subq (var y) (reg rax))))   ;;
+;; ((lambda (p)
+;;    (uncover-live p))
+;;  `(program (v w x y z)
+;;     (movq (int 1) (var v))       ;; v
+;;     (movq (int 46) (var w))      ;; v,w
+;;     (movq (var v) (var x))       ;; w,x
+;;     (addq (int 7) (var x))       ;; w,x
+;;     (movq (var x) (var y))       ;; w,x,y
+;;     (addq (int 4) (var y))       ;; w,x,y
+;;     (movq (var x) (var z))       ;; w,y,z
+;;     (addq (var w) (var z))       ;; y,z
+;;     (movq (var z) (reg rax))     ;; y,rax
+;;     (subq (var y) (reg rax))))   ;;
