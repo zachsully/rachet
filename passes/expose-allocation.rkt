d9bac80dc63545f6c76c8b1f6463dd641e304ca1
Zach Sullivan <zsulliva@indiana.edu>
2016-02-19 17:26:38 -0500

    finish expose=allocation, but noticed changes needed in flatten

--- passes/expose-allocation.rkt
+++ passes/expose-allocation.rkt
@@ -1,4 +1,59 @@
 #lang racket
 (provide expose-allocation)
 
-(define (expose-allocation p) p)
+;;
+;; Expose-Allocation
+;;
+;; expose-allocation : C3 -> C3
+;;
+;; Pass:
+;; > inserts intialize statement at the top (setting up garbage collector)
+;; > changes the form
+;;  (assign <lhs> (vector <f> .. <e>))
+;;           -to-
+;;  (if (collection-needed? <bytes>)
+;;      ((collect <bytes>))
+;;      ())
+;;  (assign <lhs> (allocate <len> <type>))
+;;  (vector-set! <lhs> 0 <f>)
+;;   ..
+;;  (vector-set! <lhs> (len -1) <e>)
+
+(define (expose-allocation p)
+  (match p
+   [`(program ,extra ,stmts ...)
+    `(program ,extra
+              ,@(foldr
+                 (lambda (stmt acc)
+                   (match stmt
+                    [`(assign ,var (vector ,es ...))
+                     (let* ([vsets (vector-setting var es)]
+                            [bytes (+ 8 (* 8 (cdr vsets)))])
+                       (append `((if (collection-needed? ,bytes)
+                                     ((collect ,bytes))
+                                     ())
+                                 (assign ,var (allocate ,(length es) 'foo))
+                                 ,@(car vsets))
+                               acc))]
+
+                    [`,s (append s acc)]))
+                 '()
+                 stmts))]))
+
+(define (vector-setting var es)
+ ((lambda (f)
+    (foldr f `(() . 0) es))
+  (lambda (e acc)
+    (cons (append (car acc)
+                  `((vector-set! ,var ,(cdr acc) ,e)))
+          (+ 1 (cdr acc))))))
+
+;; (vector-setting 'foo (range 1 10))
+;; (range 1 10)
+;; (expose-allocation
+;;  `(program (t.1 t.2 t.3 t.4)
+;;    (assign t.1 (vector 42))
+;;    (assign t.2 (vector t.1))
+;;    (assign t.3 (vector-ref t.2 0))
+;;    (assign t.4 (vector-reg t.3 0))
+;;    (return t.4)))
