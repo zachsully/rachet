09bb73e23ce8a8c4811112e63a9cad25f2b3c91e
zachsully <zachsully@gmail.com>
2016-02-21 16:08:03 -0500

    still not working select instructions

--- passes/select-instructions.rkt
+++ passes/select-instructions.rkt
@@ -25,52 +25,57 @@
 
    [`(assign ,v (- ,e1))
     (let ([v^ (select-instructions^ v rs)]
-          [e1^ (select-instructions^ e1 rs)])
+	  [e1^ (select-instructions^ e1 rs)])
       `((movq ,e1^ ,v^)
-        (negq ,v^)))]
+	(negq ,v^)))]
 
    [`(assign ,v (+ ,e1 ,e2))
     (let ([v^ (select-instructions^ v rs)]
-          [e1^ (select-instructions^ e1 rs)]
-          [e2^ (select-instructions^ e2 rs)])
+	  [e1^ (select-instructions^ e1 rs)]
+	  [e2^ (select-instructions^ e2 rs)])
       `((movq ,e2^ ,v^)
-        (addq ,e1^ ,v^)))]
+	(addq ,e1^ ,v^)))]
 
    [`(assign ,e1 (eq? ,arg1 ,arg2))
-    `((cmpq ,(select-instructions^ arg1 rs) ,(select-instructions^ arg2 rs))
+    `((cmpq ,(select-instructions^ arg1 rs)
+	    ,(select-instructions^ arg2 rs))
       (sete (byte-reg al))
       (movzbq (byte-reg al) ,(select-instructions^ e1 rs)))]
 
    [`(assign ,v (not ,es))
-    `((movq ,(select-instructions^ es rs) ,(select-instructions^ v rs))
+    `((movq ,(select-instructions^ es rs)
+	    ,(select-instructions^ v rs))
       (xorq (int 1) ,(select-instructions^ v rs)))]
 
    [`(assign ,v (allocate ,len (Vector ,ts ...)))
     (let ([v^ (select-instructions^ v rs)])
       `((movq (global-value free_ptr) ,v^)
-        (addq (int ,(* 8 (+ 1 len))) (global-value free_ptr))
-        (movq (int TAG) (offset ,v^ 0))))]
+	(addq (int ,(* 8 (+ 1 len))) (global-value free_ptr))
+	(movq (int TAG) (offset ,v^ 0))))]
 
    [`(assign ,v (vector-set! ,vec ,n ,arg))
-    `((movq ,(select-instructions^ arg)
-            (offset ,(select-instructions^ vec rs) ,(* 8 (+ 1 n)))))]
+    `((movq ,(select-instructions^ arg rs)
+	    (offset ,(select-instructions^ vec rs) ,(* 8 (+ 1 n)))))]
 
    [`(assign ,v (vector-ref ,vec ,n))
-    `((movq (offset ,(select-instructions^ vec rs) ,(* 8 (+ 1 n)))
-            ,v))]
+    `((movq (offset ,(select-instructions^ vec rs)
+		    ,(* 8 (+ 1 n)))
+	    ,v))]
 
    [`(if (eq? ,bool ,cnd) ,thn ,els)
-    `((if (eq? ,(select-instructions^ bool rs) ,(select-instructions^ cnd rs))
-          ,(select-instructions^ thn rs)
-          ,(select-instructions^ els rs)))]
+    `((if (eq? ,(select-instructions^ bool rs)
+	       ,(select-instructions^ cnd rs))
+	  ,(select-instructions^ thn rs)
+	  ,(select-instructions^ els rs)))]
 
    ;; Generic assign statement
    [`(assign ,v ,x)
     (let ([v^ (select-instructions^ v rs)]
-          [x^ (select-instructions^ x rs)])
+	  [x^ (select-instructions^ x rs)])
       `((movq ,x^ ,v^)))]
 
-   [`(return ,v) `((movq ,(select-instructions^ v rs) (reg rax)))]
+   [`(return ,v)
+    `((movq ,(select-instructions^ v rs) (reg rax)))]
 
    [`(if (collection-needed? ,bytes) ,thn ,els)
     `((movq (global-value free_ptr) (var end-data.1))
@@ -79,40 +84,44 @@
       (setl (byte-reg al))
       (movzbq (byte-reg al) (var lt.1))
       (if (eq? (int 0) (var lt.1))
-          ,(select-instructions^ els rs)
-          ,(select-instructions^ thn rs)))]
+	  ,(select-instructions^ els rs)
+	  ,(select-instructions^ thn rs)))]
 
    [`(call-live-roots ,vs (collect ,bytes))
-    (append (car (foldr (lambda (v acc)
-                     (cons (append `((movq (var ,v)
-                                     (offset (var ROOTSTACK.prev)
-                                             ,(* 8 (- 1 (cdr acc))))))
-                                   (car acc))
-                           (+ 1 (cdr acc))))
-                   '(() . 0)
-                   vs))
-            `((movq ROOTSTACK.prev ROOTSTACK.new)
-              (addq (int ,(length vs)) ROOTSTACK.new)
-              (movq (var ROOTSTACK.new) (reg rdi))
-              (movq (int ,bytes) (reg rsi))
-              (callq collect))
-            (car (foldr (lambda (v acc)
-                     (cons (append `((movq
-                                      (offset (var ROOTSTACK.prev)
-                                             ,(* 8 (- 1 (cdr acc))))
-                                      (var ,v)
-                                     ))
-                                   (car acc))
-                           (+ 1 (cdr acc))))
-                   '(() . 0)
-                   vs)))]
+    (append
+     (car (foldr (lambda (v acc)
+		   (cons (append
+			  `((movq (var ,v)
+				  (offset (var ,rs)
+					  ,(* 8 (- 1 (cdr acc))))))
+			  (car acc))
+			 (+ 1 (cdr acc))))
+		 '(() . 0)
+		 vs))
+     `((movq ,rs ROOTSTACK.new)
+       (addq (int ,(length vs)) ROOTSTACK.new)
+       (movq (var ROOTSTACK.new) (reg rdi))
+       (movq (int ,bytes) (reg rsi))
+       (callq collect))
+     (car (foldr (lambda (v acc)
+		   (cons (append
+			  `((movq
+			     (offset (var ,rs)
+				     ,(* 8 (- 1 (cdr acc))))
+			     (var ,v)
+			     ))
+			  (car acc))
+			 (+ 1 (cdr acc))))
+		 '(() . 0)
+		 vs)))]
 
    [`(initialize ,rootlen ,heaplen)
     `((movq (int ,rootlen) (reg rdi))
       (movq (int ,heaplen) (reg rsi))
       (callq initialize)
-      (movq (global-value rootstack_begin) (var rootstack)))]
+      (movq (global-value rootstack_begin) (var ,rs)))]
 
+   ;; What case does this cover?
    [`(,x ...) (select-instructions^ (car x) rs)]
    ))
 
@@ -120,8 +129,8 @@
   (lambda (e)
     (match e
      [`(program ,vs (type ,t) ,es ...)
-      `(program ,vs ,@(select-instructions^ es 'rootstack_begin))]
-     ;; ['() '()]
-     ;; [else (append (select-instructions^ (car e))
-     ;;               (select-instructions (cdr e)))]
+      `(program ,vs ,@(select-instructions es))]
+     ['() '()]
+     [else (append (select-instructions^ (car e) 'rootstack_begin)
+		   (select-instructions (cdr e)))]
      )))
